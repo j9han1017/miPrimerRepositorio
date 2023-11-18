41b4aecb5945dc05cc2a91c0935a761108a3fee5
J9han1017 <johandiaz1017@gmail.com>
2023-11-18 15:21:58 -0500

    clase docker2

--- script.js
+++ script.js
@@ -5,52 +5,23 @@
 // const fs = require('fs')
 // fs.writeFile('texto1.txt','Archivo creado desde NODE')
 // console.log('Documento creado')
-const data = {
-    "users": [
-      {
-        "id": 1,
-        "name": "Alice",
-        "age": 25,
-        "email": "alice@example.com"
-      },
-      {
-        "id": 2,
-        "name": "Bob",
-        "age": 30,
-        "email": "bob@example.com"
-      },
-      {
-        "id": 3,
-        "name": "Charlie",
-        "age": 28,
-        "email": "charlie@example.com"
-      }
-    ]
-  }
-
-const http = require('http');
-
-const PORT = 3000;
-const server = http.createServer((req, res)=>{
-if (req.method === 'GET' ){
-    const nombre = req.url.split('?')[0]
-    const edad = req.url.split('?')[1]
-    const mensajeMayor =('hola '+ nombre +' Eres mayor de edad\n')
-    const mensajeMenor =('hola '+ nombre+' Eres menor de edad\n')
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-
-   if(edad>=18){
-    res.end( );
-   }else{
-    res.end('hola '+ nombre+' Eres menor de edad\n' )
-
-   }
-    
-}else {
-    res.writeHead(404,{'Conten-Type': 'text/plain'})
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const nombre = req.url.split('?')[1];
+    const edad = req.url.split('?')[2];
+    const eleccion = req.url.split('?')[3];
+  
+    if(edad>=18) {
+      res.end('hola ' + nombre + ' eres mayor de edad\n');
+      fs.appendFileSync('texto2.txt', `${nombre} archivo creado usando librerias de NODE en ejercicio para practicas\n`)
+    } else if(eleccion == 'si'){
+      res.end('hola ' + nombre + ' eres menor de edad y no tienes permisos.\n');
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found\n');
-}    
+  }
 });
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  console.log(`Server is running on port ${PORT}`);
+});
